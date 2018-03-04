401e0403c25a80601c1aee9052850a046e63473e
Roman Yefimets <romanyefimets@wless-user-172019244215.temp.wsu.edu>
2018-03-03 21:38:14 -0800

    added forms updates

--- Scheduler/Scheduler/Form1.cs
+++ Scheduler/Scheduler/Form1.cs
@@ -8,13 +8,328 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Scheduler
 {
+
+
     public partial class Form1 : Form
     {
+        public static Color HIGHLIGHT = Color.Yellow;
+        public static Color NORMAL = Color.White;
+
+        public static int NUM_DAYS = 31;
+
+        public static Calander scheduler;
+        public static Dictionary<int, PictureBox> picMap;
+
+
+
         public Form1()
         {
             InitializeComponent();
+            scheduler = new Calander(pictureBox1);
+            picMap = new Dictionary<int, PictureBox>();
+            createPicMap();
+            populate();
+            highLight(1); // highlight current date
+           
+        }
+
+        public void createPicMap()
+        {
+
+            picMap.Add(1, pictureBox2);
+            picMap.Add(2, pictureBox3);
+            picMap.Add(3, pictureBox4);
+            picMap.Add(4, pictureBox5);
+            picMap.Add(5, pictureBox6);
+            picMap.Add(6, pictureBox7);
+            picMap.Add(7, pictureBox8);
+            picMap.Add(8, pictureBox9);
+            picMap.Add(9, pictureBox10);
+            picMap.Add(10, pictureBox11);
+
+            picMap.Add(11, pictureBox12);
+            picMap.Add(12, pictureBox13);
+            picMap.Add(13, pictureBox14);
+            picMap.Add(14, pictureBox15);
+            picMap.Add(15, pictureBox16);
+            picMap.Add(16, pictureBox17);
+            picMap.Add(17, pictureBox18);
+            picMap.Add(18, pictureBox19);
+            picMap.Add(19, pictureBox20);
+            picMap.Add(20, pictureBox21);
+
+            picMap.Add(21, pictureBox22);
+            picMap.Add(22, pictureBox23);
+            picMap.Add(23, pictureBox24);
+            picMap.Add(24, pictureBox25);
+            picMap.Add(25, pictureBox26);
+            picMap.Add(26, pictureBox27);
+            picMap.Add(27, pictureBox28);
+            picMap.Add(28, pictureBox29);
+            picMap.Add(29, pictureBox30);
+            picMap.Add(30, pictureBox31);
+
+            picMap.Add(31, pictureBox32);
+            picMap.Add(32, pictureBox33);
+            picMap.Add(33, pictureBox34);
+            picMap.Add(34, pictureBox35);
+            picMap.Add(35, pictureBox36);
+
+
+
+        }
+
+        public void populate()
+        {
+            for (int i = 1; i < NUM_DAYS; i++)
+            {
+                Day day = new Day(i);
+                scheduler.addDayMap(i, day);
+                scheduler.getDay(i).setColorBox(picMap[i]);
+                int dayNum = scheduler.getDay(i).getNumber();
+                writeDayNum(dayNum);
+                writeDayName();
+            }
+        }
+
+        private void singleClick(object sender, EventArgs e, int boxNumber)
+        {
+            if (scheduler.getDay(boxNumber) != null)
+            {
+                scheduler.prevSelected = scheduler.selected;
+                scheduler.selected = boxNumber;
+
+                highLight(scheduler.selected);
+                unHighLight(scheduler.prevSelected);
+            }
+
+        }
+        public void writeDayName()
+        {
+
+            writeDayName(pictureBox37, "Sunday");
+            writeDayName(pictureBox38, "Monday");
+            writeDayName(pictureBox39, "Tuesday");
+            writeDayName(pictureBox40, "Wednesday");
+            writeDayName(pictureBox41, "Thursday");
+            writeDayName(pictureBox42, "Friday");
+            writeDayName(pictureBox43, "Saturday");
+        }
+
+        public void writeDayName(PictureBox picBox, String name)
+        {
+            Image image = new Bitmap(picBox.Width, picBox.Height);
+            Graphics g = Graphics.FromImage(image);
+            Font font = new Font("TimesNewRoman", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+            g.DrawString(name, font, Brushes.Black, new Point(0, 0));
+            picBox.Image = image;
+        }
+
+        public void writeDayNum(int boxNumber)
+        {
+
+            Day day = scheduler.getDay(boxNumber);
+            PictureBox picBox = day.getColorBox();
+
+
+
+            Image image = new Bitmap(picBox.Width, picBox.Height);
+            Graphics g = Graphics.FromImage(image);
+            Font font = new Font("TimesNewRoman", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+            g.DrawString(day.getNumber().ToString(), font, Brushes.Black, new Point(0, 0));
+            picBox.Image = image;
+        
+        }
+
+        public void highLight(int boxNumber)
+        {
+            scheduler.getDay(boxNumber).setBoxColor(HIGHLIGHT);
+        }
+
+        public void unHighLight(int boxNumber)
+        {
+            scheduler.getDay(boxNumber).setBoxColor(NORMAL);
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 1);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 2);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 3);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 4);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 5);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 6);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 7);
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 8);
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 9);
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 10);
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 11);
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 12);
+        }
+
+        private void pictureBox14_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 13);
+        }
+
+        private void pictureBox15_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 14);
+        }
+
+        private void pictureBox16_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 15);
+        }
+
+        private void pictureBox17_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 16);
+        }
+
+        private void pictureBox18_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 17);
+        }
+
+        private void pictureBox19_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 18);
+        }
+
+        private void pictureBox20_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 19);
+        }
+
+        private void pictureBox21_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 20);
+        }
+
+        private void pictureBox22_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 21);
+        }
+
+        private void pictureBox23_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 22);
+        }
+
+        private void pictureBox24_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 23);
+        }
+
+        private void pictureBox25_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 24);
+        }
+
+        private void pictureBox26_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 25);
+        }
+
+        private void pictureBox27_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 26);
+        }
+
+        private void pictureBox28_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 27);
+        }
+
+        private void pictureBox29_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 28);
+        }
+
+        private void pictureBox30_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 29);
+        }
+
+        private void pictureBox31_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 30);
+        }
+
+        private void pictureBox32_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 31);
+        }
+
+        private void pictureBox33_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 32);
+        }
+
+        private void pictureBox34_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 33);
+        }
+
+        private void pictureBox35_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 34);
+        }
+
+        private void pictureBox36_Click(object sender, EventArgs e)
+        {
+            singleClick(sender, e, 35);
         }
     }
 }
+
+
--- Scheduler/Scheduler/Program.cs
+++ Scheduler/Scheduler/Program.cs
@@ -12,27 +12,15 @@ namespace Scheduler
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
+
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-            //BOGGIES TEST CODE
+            Application.Run(new Form1());
 
-            Year current = new Year();
-            current.setYear(2018);
-
-            TestClass test = new TestClass();
-            test.loadCalender(current);
-            test.loadEvents(current);
-
-            //string hello = "dick eater";
-            //test.addEvent(9, 4, 12.5, 14.5, hello, current);
-            //test.addEvent(10, 3, 14, 16, "PENISSS", current);
-
-            //test.SaveCalendar(current);
-            
         }
     }
 }
