dffb36e95c2d939e0f7378cc6ee6c0f41a2c6a40
boggiex17 <you@example.com>
2018-03-03 19:11:34 -0800

    save events and load them

--- Scheduler/Scheduler/Program.cs
+++ Scheduler/Scheduler/Program.cs
@@ -14,20 +14,25 @@ namespace Scheduler
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
 
-            /* BOGGIES TEST CODE
+            //BOGGIES TEST CODE
+
             Year current = new Year();
             current.setYear(2018);
 
             TestClass test = new TestClass();
             test.loadCalender(current);
+            test.loadEvents(current);
 
-            string hello = "dick eater";
-            test.addEvent(9, 4, 12.5, 14.5, hello, current);
-            */
+            //string hello = "dick eater";
+            //test.addEvent(9, 4, 12.5, 14.5, hello, current);
+            //test.addEvent(10, 3, 14, 16, "PENISSS", current);
+
+            //test.SaveCalendar(current);
+            
         }
     }
 }
